d65c12ca4b8da0641bb18f3481391a0d66dd2ded
Brant Faircloth <brant.faircloth@gmail.com>
2009-03-29 20:33:13 -0700

    Cleanup line-wrapping in msatCommand.py

--- msatCommand.py
+++ msatCommand.py
@@ -18,10 +18,29 @@ from Bio import SeqIO
 def interface():
     usage = "usage: %prog [options] source_file.fa"
     p = optparse.OptionParser(usage)
-    p.add_option('--min-length', '-m', dest='min_length', default = 6, help='The minimum length required for repeats to be considered', metavar='MIN_LENGTH')
-    p.add_option('--output-file', '-o', dest='output_file', default= None, help='The name of the output file for results [default=None]', metavar='FILE')
-    p.add_option('--scan-type', '-s', default='all', help="The type of scan to run.  Valid options are: \n(1)'all' to search for everything\n '1' | '2' | '3 | '4' | '5' | '6' to search for only mononuc, dinuc, trinuc, etc.\n'2+' | '3+' | '4+' | '5+' | '6+' to search for >= dinuc, >= trinuc, etc. [default=all]")
-    p.add_option('--perfect-repeats', '-p', dest='perfect', action='store_true', default=False, help="Locate only perfect repeats.  The default of false only means that when you have a repeat like 'ACACACACACACNNAC', where ambiguous bases are equal to the exact length of the repeat motif (in this case 2), ambiguous bases will be counted as part of the repeat.  This is true IFF the ambiguous bases are followed by >=1 repeat motif, also as in the example [default=False]")
+    
+    p.add_option('--min-length', '-m', dest='min_length', default = 6, help=\
+    'The minimum length required for repeats to be considered', metavar=\
+    'MIN_LENGTH')
+    
+    p.add_option('--output-file', '-o', dest='output_file', default= None, \
+    help='The name of the output file for results [default=None]', metavar=\
+    'FILE')
+    
+    p.add_option('--scan-type', '-s', default='all', help="The type of scan \
+to run.  Valid options are:  (1)'all' to search for everything '1' | \
+'2' | '3 | '4' | '5' | '6' to search for only mononuc, dinuc, trinuc, \
+etc. '2+' | '3+' | '4+' | '5+' | '6+' to search for \
+>= dinuc, >= trinuc, etc. [default=all]")
+    
+    p.add_option('--perfect-repeats', '-p', dest='perfect', action=\
+'store_true', default=False, help="Locate only perfect repeats.  The \
+default of false only means that when you have a repeat like \
+'ACACACACACACNNAC', where ambiguous bases are equal to the exact length \
+of the repeat motif (in this case 2), ambiguous bases will be counted as \
+part of the repeat.  This is true IFF the ambiguous bases are followed \
+by >=1 repeat motif, also as in the example [default=False]")
+    
     (options,arg) = p.parse_args()
     if not arg:
         p.print_help()
@@ -32,20 +51,26 @@ def createMotifInstances(motif, min_length, perfect):
     return seqsearch.MicrosatelliteMotif(motif, min_length, perfect)
 
 def genMotifCollection(options):
-    possible_motifs = (motif.mononucleotide, motif.dinucleotide, motif.trinucleotide, motif.tetranucleotide, motif.pentanucleotide, motif.hexanucleotide)
+    possible_motifs = (motif.mononucleotide, motif.dinucleotide, \
+    motif.trinucleotide, motif.tetranucleotide, motif.pentanucleotide, \
+    motif.hexanucleotide)
+    
     motif_collection = ()
     if options.scan_type == 'all':
         for m in possible_motifs:
-            motif_collection += (createMotifInstances(m, options.min_length, options.perfect),)
+            motif_collection += (createMotifInstances(m, options.min_length, \
+            options.perfect),)
     elif '+' in options.scan_type:
         # subtracting 1 so that we get >= options.scan_type
         scan = int(options.scan_type[0]) - 1
         for m in possible_motifs[scan:]:
-            motif_collection += (createMotifInstances(m, options.min_length, options.perfect),)
+            motif_collection += (createMotifInstances(m, options.min_length, \
+            options.perfect),)
     else:
         # no iteration here because tuple != nested
         scan = int(options.scan_type[0]) - 1
-        motif_collection += (createMotifInstances(possible_motifs[scan], options.min_length, options.perfect),)
+        motif_collection += (createMotifInstances(possible_motifs[scan], \
+        options.min_length, options.perfect),)
     return motif_collection
 
 def stdOut(name, matches):
@@ -55,10 +80,12 @@ def stdOut(name, matches):
                 start = match[0]
                 end = match[1]
                 length = (end - start) / len(msat)
-                print '%s:\t(%s)^%s\trepeat between bases\t%s\tand\t%s' % (name, msat, length, start, end)
+                print '%s:\t(%s)^%s\trepeat between bases\t%s\tand\t%s' % \
+                (name, msat, length, start, end)
     else:
-        print '%s:\t No microsatellites found (or repeat region < min_length)' % name
-                
+        print '%s:\t No microsatellites found (or repeat region < \
+min_length)' % name
+
 def main():
     start_time = time.time()
     options,arg = interface()
@@ -73,7 +100,7 @@ def main():
     end_time = time.time()
     if not options.output_file:
         print '\nTime for execution: ', end_time - start_time, 'seconds'
-        
+
 
 if __name__ == '__main__':
     main()
